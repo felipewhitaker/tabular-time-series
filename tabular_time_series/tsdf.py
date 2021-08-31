71afca4c1cfa7749031ac6936fadf5291dddcfc1
Felipe Whitaker <felipewhitaker@gmail.com>
2021-08-30 22:47:06 -0300

    Update format using black

--- tabular_time_series/tsdf.py
+++ tabular_time_series/tsdf.py
@@ -1,9 +1,12 @@
 from .tsgenerator import TimeSeriesGenerator
-import pandas as pd # type: ignore
+import pandas as pd  # type: ignore
 from typing import Union
 import numpy as np
 
-def timeseries2df(data: Union[np.array], p: int, n: int, s: int = -1) -> pd.DataFrame: #, category_seasonal, test_split = .33):
+
+def timeseries2df(
+    data: Union[np.array], p: int, n: int, s: int = -1
+) -> pd.DataFrame:  # , category_seasonal, test_split = .33):
     """Interface for `TimeSeriesGenerator`, transforming the generator into a `pd.DataFrame`
 
     Returns:
@@ -11,27 +14,25 @@ def timeseries2df(data: Union[np.array], p: int, n: int, s: int = -1) -> pd.Data
         n int: lenght of `y`
         s int: seasonal parameter
     """
-    
+
     # Define ts
     ts = TimeSeriesGenerator(data, p, n, s)
-    
+
     # Iterate over
     features, target = [], []
     for i, (X, y) in enumerate(ts):
         features.append(X)
         target.append(y)
-        
+
     # Set columns
-    s_cols = [f'y(ts{s})_{i}' for i in range(1, n + 1)] if s > 0 else []
-    x_cols = s_cols + [f'y(t-{i})' for i in range(p)][::-1]
-    y_cols = [f'y(t+{i})' for i in range(1, n + 1)]
-    
+    s_cols = [f"y(ts{s})_{i}" for i in range(1, n + 1)] if s > 0 else []
+    x_cols = s_cols + [f"y(t-{i})" for i in range(p)][::-1]
+    y_cols = [f"y(t+{i})" for i in range(1, n + 1)]
+
     # Create DataFrame
-    dftime = pd.concat([
-        pd.DataFrame(features, columns = x_cols),
-        pd.DataFrame(target, columns = y_cols)
-    ], 
-        axis = 1
+    dftime = pd.concat(
+        [pd.DataFrame(features, columns=x_cols), pd.DataFrame(target, columns=y_cols)],
+        axis=1,
     )
-    
+
     return dftime
--- tabular_time_series/tsgenerator.py
+++ tabular_time_series/tsgenerator.py
@@ -2,23 +2,24 @@ from collections.abc import Iterator
 import numpy as np
 from typing import Union, List, Tuple
 
+
 class TimeSeriesGenerator(Iterator):
-    
-    def __init__(self, data: Union[list, np.array], p: int, n: int, s: int = -1) -> \
-        Tuple[
-            Union[List[int], List[np.int64], List[float], List[np.float64]], 
-            Union[List[int], List[np.int64], List[float], List[np.float64]]
-        ]:
+    def __init__(
+        self, data: Union[list, np.array], p: int, n: int, s: int = -1
+    ) -> Tuple[
+        Union[List[int], List[np.int64], List[float], List[np.float64]],
+        Union[List[int], List[np.int64], List[float], List[np.float64]],
+    ]:
         """
-        Iterator that receives `time series data` (rows, cols) and can 
+        Iterator that receives `time series data` (rows, cols) and can
         be iterated over, returning `X` like ([s + n] + p,) and `y` like
         (n,) such that:
-        
+
         >> np.where(y == y[0])[0].item() - np.where(X == X[0])[0].item()  == s
         >> np.where(y == y[0])[0].item() - np.where(X == X[-1])[0].item() == 1
-        
-        Thus 
-        
+
+        Thus
+
         >> data = np.array([0, 1, 2, 3, 4, 5, 6])
         >> p, n = 2, 2
         >> ts = TimeSeries(data, p, n)
@@ -43,57 +44,61 @@ class TimeSeriesGenerator(Iterator):
             [0 1 2 3] [4 5]
             (4,) (2,)
             [1 2 3 4] [5 6]
-        
+
         Parameters:
         ----------
         data : np.array of shape (n,)
         p : int
         n : int
         s : int
-        
+
         Returns:
         -------
         None
         """
-        
+
         super().__init__()
-        
+
         # assert (len(data) - n - s) // p >= 1, "There should be at least one `p` length array to be returned"
         assert n >= 1, "`n` was set to zero: shouldn't some `y` be predicted?"
-        assert p >= 0 or s >= 0, "Neither `p` nor `s` were set: which data will be used to predict `y`?"
-        assert len(data) - n - p - n * (s if s > 0 else 0) >= 0, "Impossible to generate even one instance"
-        
+        assert (
+            p >= 0 or s >= 0
+        ), "Neither `p` nor `s` were set: which data will be used to predict `y`?"
+        assert (
+            len(data) - n - p - n * (s if s > 0 else 0) >= 0
+        ), "Impossible to generate even one instance"
+
         self.S = -1
         if s > 0:
             assert s >= p + n, "`s` and `p` values shouldn't be overlapping"
             self.S = s
-        
+
         self.data = data
-        
+
         self.p = p
         self.n = n
-        
+
         self.s = 0
-        
-        self.curr = s - p if s > 0 else 0 # s - p if s > p else 0
+
+        self.curr = s - p if s > 0 else 0  # s - p if s > p else 0
         return
-    
+
     def __len__(self):
         return len(self.data) - self.n - self.p
-    
+
     def __getitem__(self, i):
         if self.curr > len(self):
             raise IndexError
-            
+
         ys = []
         if self.S > -1:
-            ys = self.data[self.s: self.s + self.n]
+            ys = self.data[self.s : self.s + self.n]
             self.s += 1
-        
-        ar = self.data[i: i + self.p] # autoregressive
-        yt = self.data[i + self.p: i + self.p + self.n] # y for prediction
+
+        ar = self.data[i : i + self.p]  # autoregressive
+        yt = self.data[i + self.p : i + self.p + self.n]  # y for prediction
         return ys + ar, yt
-    
+
     def __next__(self):
         try:
             X, y = self[self.curr]
