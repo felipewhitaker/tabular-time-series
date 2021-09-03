679fd39012d17a3dd6c3e16aaa981e2e742e28e5
Felipe Whitaker <felipewhitaker@gmail.com>
2021-09-03 18:39:25 -0300

    Add support for any Sequence structure

--- tabular_time_series/tsdf.py
+++ tabular_time_series/tsdf.py
@@ -19,19 +19,24 @@ def timeseries2df(
     ts = TimeSeriesGenerator(data, p, n, s)
 
     # Iterate over
-    features, target = [], []
-    for i, (X, y) in enumerate(ts):
-        features.append(X)
+    seasonal, features, target = [], [], []
+    for i, (sar, ar, y) in enumerate(ts):
+        seasonal.append(sar)
+        features.append(ar)
         target.append(y)
 
     # Set columns
     s_cols = [f"y(ts{s})_{i}" for i in range(1, n + 1)] if s > 0 else []
-    x_cols = s_cols + [f"y(t-{i})" for i in range(p)][::-1]
+    x_cols = [f"y(t-{i})" for i in range(p)][::-1]
     y_cols = [f"y(t+{i})" for i in range(1, n + 1)]
 
     # Create DataFrame
     dftime = pd.concat(
-        [pd.DataFrame(features, columns=x_cols), pd.DataFrame(target, columns=y_cols)],
+        [
+            pd.DataFrame(seasonal, columns=s_cols),
+            pd.DataFrame(features, columns=x_cols),
+            pd.DataFrame(target, columns=y_cols),
+        ],
         axis=1,
     )
 
--- tabular_time_series/tsgenerator.py
+++ tabular_time_series/tsgenerator.py
@@ -1,11 +1,9 @@
 from collections.abc import Iterator
-from typing import Union, List, Tuple
+from typing import Sequence, Tuple
 
 
 class TimeSeriesGenerator(Iterator):
-    def __init__(
-        self, data: Union[list], p: int, n: int, s: int = -1
-    ) -> Tuple[List, List]:
+    def __init__(self, data: Sequence, p: int, n: int, s: int = -1) -> None:
         """
         Iterator that receives `time series data` (rows, cols) and can
         be iterated over, returning `X` like ([s + n] + p,) and `y` like
@@ -49,23 +47,33 @@ class TimeSeriesGenerator(Iterator):
 
         super().__init__()
 
-        assert n >= 1, "`n` was set to zero: shouldn't some `y` be predicted?"
         assert (
-            p >= 0 or s >= 0
-        ), "Neither `p` nor `s` were set: which data will be used to predict `y`?"
+            s > 0 or p > 0
+        ) and n > 0, f"Please set (s = {s} > 0 or p = {p} > 0) and {n} > 0"
         assert (
-            len(data) - n - p - (n if s > 0 else 0) >= 0
-        ), "Impossible to generate even one instance"
+            len(data) > s
+        ), f"Sequence lenght len(data) = {len(data)} must be greater than seasonal component s = {s}"
         assert (
-            len(data) - s > 0
-        ), f"Given data of len {len(data)} can not generate seasonal of {s}"
+            len(data) >= n + p
+        ), f"Impossible to generate sequence from given parameters p = {p}, n = {n}, s = {s} for sequence of lenght len(data) = {len(data)}"
+
+        # assert n >= 1, "`n` was set to zero: shouldn't some `y` be predicted?"
+        # assert (
+        #     p >= 0 or s >= 0
+        # ), "Neither `p` nor `s` were set: which data will be used to predict `y`?"
+        # assert (
+        #     len(data) - n - p - (n if s > 0 else 0) >= 0
+        # ), "Impossible to generate even one instance"
+        # assert (
+        #     len(data) - s > 0
+        # ), f"Given data of len {len(data)} can not generate seasonal of {s}"
 
         self.S = -1
         if s > 0:
             assert s >= p + n, "`s` and `p` values shouldn't be overlapping"
             self.S = s
 
-        self.data = list(data)
+        self.data = data
 
         self.p = p
         self.n = n
@@ -75,26 +83,56 @@ class TimeSeriesGenerator(Iterator):
         self.curr = s - p if s > 0 else 0
         return
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.data) - self.n - self.p
 
-    def __getitem__(self, i):
+    def __getitem__(self, i) -> Tuple[Sequence, Sequence, Sequence]:
         if self.curr > len(self):
             raise IndexError
 
-        ys = []
+        ys = self.data[0:0]  # empty
         if self.S > -1:
             ys = self.data[self.s : self.s + self.n]
             self.s += 1
 
         ar = self.data[i : i + self.p]  # autoregressive
         yt = self.data[i + self.p : i + self.p + self.n]  # y for prediction
-        return ys + ar, yt
+        return ys, ar, yt
 
-    def __next__(self):
+    def __next__(self) -> Tuple[Sequence, Sequence, Sequence]:
         try:
-            X, y = self[self.curr]
+            s, ar, y = self[self.curr]
         except IndexError:
             raise StopIteration()
         self.curr += 1
-        return X, y
+        return s, ar, y
+
+
+if __name__ == "__main__":
+
+    import numpy as np
+    from numpy import array, int64
+
+    test_data_list = list(range(10))
+    test_data_numpy = np.arange(10)
+
+    order, yorder, sorder = 1, 1, 0
+    res = list(TimeSeriesGenerator(test_data_numpy, order, yorder, sorder))
+    expected = np.array((array([], dtype=int64), array([0]), array([1])))
+    print(np.array(res[0]) == expected)
+
+    # for test_data, name_data in [
+    #     (test_data_list, "test_data_list"),
+    #     (test_data_numpy, "test_data_numpy"),
+    # ]:
+    #     for order, yorder, sorder in [
+    #         # (len(test_data) - 1, 1, 0),
+    #         # (1, len(test_data) - 1, 0),
+    #         # (1, len(test_data) - 1, 0),
+    #         # (0, 1, len(test_data) - 1),
+    #         (1, 1, 0),
+    #     ]:
+
+    #         res = list(TimeSeriesGenerator(test_data, order, yorder, sorder))
+
+    #         print(f"({name_data}, {order}, {yorder}, {sorder}, {res}),")
